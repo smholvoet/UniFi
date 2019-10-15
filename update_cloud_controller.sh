6652d3f2093a3753f672bb01caf3644874ce71f1
Sander Holvoet <holvoetsander@hotmail.com>
2019-10-15 21:00:30 +0200

    Create update_cloud_controller.sh

---
+++ update_cloud_controller.sh
@@ -0,0 +1,11 @@
+#show unifi package
+apt-cache show unifi
+
+#update packages list
+sudo apt-get update
+
+#upgrade unifi package
+sudo apt-get install --only-upgrade unifi
+
+#check status
+sudo service unifi status
